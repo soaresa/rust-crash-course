b02e9f25a85b67dc81f8caefe0d3a85aa3685a47
Adalberto Soares <junynes@gmail.com>
2021-05-10 11:29:04 -0300

    Adds if statements

---
+++ logical-conditions/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let n = 15;
+    
+    if n < 30 {
+        println!("The number is less than 30.")
+    } else {
+        println!("The number is greater or equal to 30.")
+    }
+
+    if n == 15 {
+        println!("The number equals 15.")
+    }
+
+    if n != 20 {
+        println!("The number does not equals to 20.")
+    } else if n > 20 {
+        println!("The number is greater than 20.")
+    }
+
+
+}
