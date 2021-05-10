6165c3108ebbe7f829bab2b07b09404093e154ae
Adalberto Soares <junynes@gmail.com>
2021-05-10 09:35:53 -0300

    Adds comments

--- app-hello-world-cargo/src/main.rs
+++ app-hello-world-cargo/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
+    // 1. the macro below prints 'Hello, octopus!' to the console
+    // 2. this is another comment
+
+    /*
+    This is a 
+    multiline
+    comments
+    */
     println!("Hello, octopus!");
 }
