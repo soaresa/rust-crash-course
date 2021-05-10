1d71df9056c62b5bc628e1776377dca5ac1db1ae
Adalberto Soares <junynes@gmail.com>
2021-05-10 11:19:55 -0300

    Adds variables data types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,10 +4,20 @@ fn main() {
     To make possible change a variable you must use the keyword "mut" before the variable name
     */
     let mut x = 21;
-
     println!("The value of x is {}", x);
-
     x = 42;
-
     println!("And now is {}", x);
+
+    // Data types
+    let y = 66; // data type i32 (integer 32 bit)
+    let w: i64 = 66; // data type i64 (integer 64 bit)
+    let k: u64 = 66; // data type u64 (unsigned integer 64 bit)
+    /*unsigned integer does not support negative numbers*/
+    let f = 6.3; // f32
+    let fb: f64 = 6.6;
+    let is: bool = true;
+    let not = false;
+
+
+
 }
