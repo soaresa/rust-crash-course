fa3f7a0b2d91d789aee51c9cdb2731eb67f18254
Adalberto Soares <junynes@gmail.com>
2021-05-11 12:16:35 -0300

    Reads file

---
+++ read-file/src/main.rs
@@ -0,0 +1,11 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let mut file = File::open("info.txt").expect("Can't open the file!");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Oops! Can not read the file...");
+    
+    println!("File contents:\n\n{}", contents);
+}
